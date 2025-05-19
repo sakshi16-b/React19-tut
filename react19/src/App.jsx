afd0641e05936b5b23ca65fe88ef9f2d245d02ff
sakshi16-b <sakshibisht016@gmail.com>
2025-05-19 17:03:37 +0000

    dynamic-conditional-styling-done

--- react19/src/App.jsx
+++ react19/src/App.jsx
@@ -1,57 +1,38 @@
+import { useState } from "react";
+
 function App() {
-  const cardStyle = {
+  const [cardStyle, setCardStyle] = useState({
     border: "1px solid #cccccc#3b",
     width: "200px",
     boxShadow: "1px 2px 3px 1px #cccccc57",
     margin: "10px",
+  });
+  const [textColor, setTextColor] = useState("green");
+  const [grid, setGrid] = useState(true);
+
+  const updateTheme = (bgColor, textColor) => {
+    console.log(bgColor);
+    setCardStyle({ ...cardStyle, backgroundColor: bgColor });
+    setTextColor(textColor);
   };
+
   return (
     <>
       <h1 style={{ color: "red" }}>Inline Style in React</h1>
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="img"
-          />
-          <div style={{ padding: "5px" }}>
-            <h4>Sakshi Bisht</h4>
-            <p>Software Developer</p>
-          </div>
-        </div>
+      <button onClick={() => updateTheme("white", "black")}>
+        Default Theme
+      </button>
+      <button onClick={() => updateTheme("#aaa", "red")}>Grey Theme</button>
+      <button onClick={() => setGrid(!grid)}>Toggle Grid</button>
 
+      <div style={{ display: grid ? "flex" : "block", flexWrap: "wrap" }}>
         <div style={cardStyle}>
           <img
             style={{ width: "200px" }}
             src="https://www.w3schools.com/howto/img_avatar.png"
             alt="img"
           />
-          <div style={{ padding: "5px" }}>
-            <h4>Sakshi Bisht</h4>
-            <p>Software Developer</p>
-          </div>
-        </div>
-
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="img"
-          />
-          <div style={{ padding: "5px" }}>
-            <h4>Sakshi Bisht</h4>
-            <p>Software Developer</p>
-          </div>
-        </div>
-
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="img"
-          />
-          <div style={{ padding: "5px" }}>
+          <div style={{ padding: "5px", color: textColor }}>
             <h4>Sakshi Bisht</h4>
             <p>Software Developer</p>
           </div>
@@ -62,31 +43,7 @@ function App() {
             src="https://www.w3schools.com/howto/img_avatar.png"
             alt="img"
           />
-          <div style={{ padding: "5px" }}>
-            <h4>Sakshi Bisht</h4>
-            <p>Software Developer</p>
-          </div>
-        </div>
-
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="img"
-          />
-          <div style={{ padding: "5px" }}>
-            <h4>Sakshi Bisht</h4>
-            <p>Software Developer</p>
-          </div>
-        </div>
-
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="img"
-          />
-          <div style={{ padding: "5px" }}>
+          <div style={{ padding: "5px", color: textColor }}>
             <h4>Sakshi Bisht</h4>
             <p>Software Developer</p>
           </div>
@@ -97,7 +54,7 @@ function App() {
             src="https://www.w3schools.com/howto/img_avatar.png"
             alt="img"
           />
-          <div style={{ padding: "5px" }}>
+          <div style={{ padding: "5px", color: textColor }}>
             <h4>Sakshi Bisht</h4>
             <p>Software Developer</p>
           </div>
@@ -108,7 +65,62 @@ function App() {
             src="https://www.w3schools.com/howto/img_avatar.png"
             alt="img"
           />
-          <div style={{ padding: "5px" }}>
+          <div style={{ padding: "5px", color: textColor }}>
+            <h4>Sakshi Bisht</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={{ width: "200px" }}
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="img"
+          />
+          <div style={{ padding: "5px", color: textColor }}>
+            <h4>Sakshi Bisht</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={{ width: "200px" }}
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="img"
+          />
+          <div style={{ padding: "5px", color: textColor }}>
+            <h4>Sakshi Bisht</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={{ width: "200px" }}
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="img"
+          />
+          <div style={{ padding: "5px", color: textColor }}>
+            <h4>Sakshi Bisht</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={{ width: "200px" }}
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="img"
+          />
+          <div style={{ padding: "5px", color: textColor }}>
+            <h4>Sakshi Bisht</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={{ width: "200px" }}
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="img"
+          />
+          <div style={{ padding: "5px", color: textColor }}>
             <h4>Sakshi Bisht</h4>
             <p>Software Developer</p>
           </div>
