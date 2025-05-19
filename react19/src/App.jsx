91bcd30c54708b23c82fadb9f9c77116d642529a
sakshi16-b <sakshibisht016@gmail.com>
2025-05-19 11:27:40 +0000

    done

--- react19/src/App.jsx
+++ react19/src/App.jsx
@@ -2,37 +2,18 @@ import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Counter from "./Counter";
 
 function App() {
   const [count, setCount] = useState(0);
   const [data, setData] = useState(0);
-  useEffect(() => {
-    counterFunction();
-  }, [count]);
-
-  useEffect(() => {
-    console.log("call once");
-  }, []);
-
-  function counterFunction() {
-    console.log("counterFunction", count);
-  }
-  function callOnce() {
-    console.log("call once");
-  }
 
   return (
     <>
+      <h1>Handle props side Effect with useEffect Hook </h1>
+      <Counter count={count} data={data} />
       <button onClick={() => setCount(count + 1)}>Counter</button>
-      <br></br>
-      <br></br>
-      <br></br>
       <button onClick={() => setData(data + 1)}>Data</button>
-      <br></br>
-      <br></br>
-      Count: {count}
-      <hr></hr>
-      Data:{data}
     </>
   );
 }
