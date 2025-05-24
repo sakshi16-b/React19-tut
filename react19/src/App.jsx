4d8a95dbccf6b98c39942c90a341de4c520d0545
Sakshi-Bisht <sakshibisht016@gmail.com>
2025-05-24 07:08:43 +0000

    bootstrap-styling

--- react19/src/App.jsx
+++ react19/src/App.jsx
@@ -1,48 +1,46 @@
 import { useState, useEffect } from "react";
 import styled from "styled-components";
-
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import "./App.css";
 
 function App() {
-  //1 method to write styled component
-  // const Heading = styled.h1`
-  //   color: red;
-  //   border: 1px solid red;
-  //   border-radius: 5px;
-  //   margin: 20px;
-  //   padding: 20px;
-  //  `;
-
-  //2 method to write styled Component
-
-  const Heading = styled.h1({
-    color: "red",
-    border: "1px solid green",
-    borderRadius: "5px",
-    margin: "20px",
-    padding: "20px",
-  });
-
-  const Btn = styled.button`
-    color: red;
-    background-color: #ddd;
-    border: 1px solid green;
-    margin: 20px;
-  `;
-  const SignupBtn = styled.button({
-    color: "red",
-    backgroundColor: "grey",
-  });
-  const Para = styled.p({
-    color: "red"
-})
   return (
     <>
-      <h1>Styled Components</h1>
-      <Heading>Hello Heading</Heading>
-      <Btn>Login</Btn>
-      <SignupBtn>SignUp</SignupBtn>
-      <Para>Hello I am Paragraph</Para>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Button>ADD</Button>
+      <Alert variant={"danger"}>This is danger Alert</Alert>
+      <Alert variant={"success"}>This is Success Alert</Alert>
+      <button onClick={() => alert("Simple Btn")}>Simple Button</button>
+      <Button onClick={() => alert("Bootstrap Btn")}>Bootstrap Button</Button>
     </>
   );
 }
