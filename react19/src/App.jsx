b557be0baf1bd6e3ba4099ca78f96795ee104932
Sakshi-Bisht <sakshibisht016@gmail.com>
2025-05-24 16:29:18 +0000

    done-uncontrolled comp

--- react19/src/App.jsx
+++ react19/src/App.jsx
@@ -1,34 +1,47 @@
 import { useRef } from "react";
-import "./App.css";
 
 function App() {
-  const inputRef = useRef();
-  const h1Ref = useRef();
-  function handleSubmit() {
-    console.log(inputRef);
-    inputRef.current.style.color = "red";
-    inputRef.current.placeholder = "enter password";
-  }
+  const userRef = useRef();
+  const passwordRef = useRef();
 
-  const toggleHandle = () => {
-    if (inputRef.current.style.display != "none")
-      inputRef.current.style.display = "none";
-    else {
-      inputRef.current.style.display = "inline";
-    }
+  const handleForm = (e) => {
+    e.preventDefault();
+    const user = document.querySelector("#user").value;
+    const password = document.querySelector("#password").value;
+    console.log(user, password);
+  };
+  const handleFormRef = (e) => {
+    e.preventDefault();
+    const user = userRef.current.value;
+    const password = passwordRef.current.value;
+    console.log(user, password);
   };
 
-  function handleAdd() {
-  h1Ref.current.style.color="green"
-  }
   return (
     <>
-      <h1>useRef Hook(Uncontrolled Component)</h1>
-      <input type="text" placeholder="Enter username" ref={inputRef} />
-      <button onClick={handleSubmit}>Submit</button>
-      <button onClick={toggleHandle}>Toggle</button>
-      <h1 ref={h1Ref}>Heading Ref</h1>
-      <button onClick={handleAdd}>Add</button>
+      <h1>Uncontrolled Component</h1>
+
+      <form method="" action="post" onSubmit={handleForm}>
+        <input type="text" id="user" placeholder="Enter Username" />
+        <br />
+        <br />
+        <input type="text" id="password" placeholder="Enter Userpassword" />
+        <br />
+        <br />
+        <button>Submit</button>
+      </form>
+
+      <hr />
+      <h1>Uncontrolled Component using useRef</h1>
+      <form method="" action="post" onSubmit={handleFormRef}>
+        <input type="text" placeholder="Enter Username" ref={userRef} />
+        <br />
+        <br />
+        <input type="text" placeholder="Enter Userpassword" ref={passwordRef} />
+        <br />
+        <br />
+        <button>Submit with Ref</button>
+      </form>
     </>
   );
 }
