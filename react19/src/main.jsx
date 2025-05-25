2da90bd8d9e255d548f6705e62f63253e9812a86
Sakshi-Bisht <sakshibisht016@gmail.com>
2025-05-25 07:38:47 +0000

    passing function as a props

--- react19/src/main.jsx
+++ react19/src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
   /* The following line can be included in your src/index.js or App.js file */
 }
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+//import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
