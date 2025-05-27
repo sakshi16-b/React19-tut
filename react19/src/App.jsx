cdda7fc0e871ffb80728870f2a1bd297d7146883
Sakshi-Bisht <sakshibisht016@gmail.com>
2025-05-27 15:47:23 +0000

    derived state

--- react19/src/App.jsx
+++ react19/src/App.jsx
@@ -1,36 +1,16 @@
 import { useState } from "react";
+import AddUser from "./AddUser";
+import DisplayUser from "./DisplayUser";
 
 function App() {
-  const [user, setUser] = useState(0);
-  const [users, setUsers] = useState([]);
-
-  const addUsers = () => {
-    setUsers([...users, user]);
-  };
+  const [user, setUser] = useState("");
   return (
     <>
-      <h1>Derived State in React Js</h1>
-      <h4>Total Users:{users.length}</h4>
-      <h4>Last User:{users[users.length - 1]}</h4>
-      <h4>Total Unique Users: {[...new Set(users)].length}</h4>
-      <br />
-
-      <input
-        type="text"
-        placeholder="Enter User"
-        onChange={(e) => setUser(e.target.value)}
-        value={user}
-      />
-      <br />
-      <br />
-      <button onClick={addUsers}>Add User</button>
-      <br />
-      <br />
-      <br />
-      {users.map((user, index) => {
-        return <h4 key={index}>{user}</h4>;
-      })}
+      <h1>Lifting State Up</h1>
+      <AddUser setUser={setUser} />
+      <DisplayUser user={user} />
     </>
   );
 }
+
 export default App;
---
+++ react19/src/DisplayUser.jsx
@@ -0,0 +1,4 @@
+function DisplayUser({user}) {
+  return <h4>Display User:{user}</h4>;
+}
+export default DisplayUser;
